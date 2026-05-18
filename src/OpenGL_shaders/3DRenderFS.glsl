99304046946421fa7cde9206c4da62d3c027cd33
KojoZero <kwaku.kiriguya@gmail.com>
2026-05-18 04:31:28 -0700

    fixed shader for rgba8

--- src/OpenGL_shaders/3DRenderFS.glsl
+++ src/OpenGL_shaders/3DRenderFS.glsl
@@ -60,7 +60,7 @@ vec4 FinalColor()
         vec3 texcoord = vec3(fTexcoord, fPolygonAttr.y);
         vec4 tcol;
         if (fPolygonAttr.z == 0)
-            tcol = texture(CurTexture, texcoord);
+            tcol = (vec4(texture(CurTexture, texcoord)) * 255.0) / vec4(63,63,63,31);
         else if (fPolygonAttr.z == 1)
             tcol = texture(Capture128Texture, texcoord);
         else
